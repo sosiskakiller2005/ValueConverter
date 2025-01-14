9668862e8ebb11877b6cef6c7596840bd95d8ca6
Козлов.Владислав <Vkozlov15012005@gmail.com>
2025-01-14 14:11:41 +0300

    Добавьте файлы проекта.

---
+++ ValueConverter/Model/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueConverter.Model
+{
+    public class Currency
+    {
+        public DateTime Date { get; set; }
+        public DateTime PreviousDate { get; set; }
+        public string PreviousURL { get; set; }
+        public DateTime Timestamp { get; set; }
+        public Dictionary<string, Valute> Valute { get; set; }
+
+
+
+    }
+}
