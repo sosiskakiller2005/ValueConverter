f3b39b0b99ef5cbdf82d02bf70d871a2c7888d35
Козлов.Владислав <Vkozlov15012005@gmail.com>
2025-01-14 14:36:05 +0300

    asd

--- ValueConverter/View/Windows/MainWindow.xaml.cs
+++ ValueConverter/View/Windows/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ValueConverter.Model;
+using static System.Net.WebRequestMethods;
 
 namespace ValueConverter
 {
@@ -20,9 +24,24 @@ namespace ValueConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string JSON_PATH = "https://www.cbr-xml-daily.ru/daily_json.js";
         public MainWindow()
         {
             InitializeComponent();
+            LoadCurrencyAsync();
         }
+
+        async Task LoadCurrencyAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(JSON_PATH);
+
+            if (!string.IsNullOrEmpty(response))
+            {
+                Currency currency = JsonConvert.DeserializeObject<Currency>(response);
+
+                CurrencyLV.ItemsSource = currency.Valute.Values;
+            }
+        } 
     }
 }
